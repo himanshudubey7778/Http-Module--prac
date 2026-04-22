013386d30a9423a84acc7130c72e7e960f614161
himanshudubey <hackerhimanshudubey7778@gmail.com>
2026-04-22 09:21:03 +0530

    FSD1NODE update

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const http = require('http'); // 1. Module ko bulaya
+const http = require('http');
+const fs = require('fs'); // Files read karne ke liye
 
-// 2. Server banaya
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'}); // Sab sahi hai (Status 200)
-  res.end('Himanshu Dubey, your server is Live!'); // Browser ko jawab bheja
+    // HTML file ko read karo
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.write('File Not Found!');
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data); // HTML ka data browser ko bhejo
+        }
+        res.end();
+    });
 });
 
-// 3. Server ko ek "Port" par bithaya
 server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+    console.log('Tejjora Server: http://localhost:3000 server live is');
 });
